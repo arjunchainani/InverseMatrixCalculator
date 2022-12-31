a180ce7d2767c0f54ae55548986ce1f3ab23f23f
ArjunChainani <arjun.k.chainani@gmail.com>
2022-12-30 22:12:42 -0500

    working on result vector converter

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ numVars = int(input('How many variables are in your system? '));
 equations = []
 
 for i in range(numVars):
-    equations.append(input('Please enter the equation #' + str(i + 1) + ' here: '))
+    equations.append(input('Please enter equation #' + str(i + 1) + ' here: '))
 
 origMatrix = matrix.MatrixConverter(numVars, equations)
 print(origMatrix)
